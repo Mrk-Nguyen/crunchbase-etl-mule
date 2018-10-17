8a4e4b4897a9a02b7801130377871f36f889565a
Mark Nguyen <Mrk-Nguyen@users.noreply.github.com>
2018-10-17 12:04:44 -0700

    initial

---
+++ preprocess/crunchCompanies.py
@@ -0,0 +1,89 @@
+companies = ('marketo','sitecore','sailthru','agilone','searchignite','neustar',
+'tealium','ensighten','v12-group','terdata','idg-ventures','forrester-research',
+'the-walt-disney-company','mondelez-international','edmunds-com','eyeota','stirista',
+'conagra-inc','interactive-advertising-bureau-iab','nikkei','adroll-semantic-sugar-inc',
+'demandmedia','a-e-television-networks','vox-media','gartner','jetblue-airways','kellogg'
+)
+
+companies2 = ('bazaarvoice','centro','digital-trends','e-trade','financial-times',
+'foxtel-management-pty-ltd','the-guardian','dailymotion')
+
+companies3 = ('turner-broadcasting','warner-bros','roku','sling-com','fandango',
+'flixster','dramafever','bleacher-report','istreamplanet','refinery29','mashable')
+
+companies4 = ('le-figaro','keurig','mitsubishi','rea-group',
+'shutterstock','the-enthusiast-network','time','tribunecompany','wenner-media',
+'whosay','wikia','choicestream','videoamp','admobius','idg-enterprise',
+'accel-partners','firefox')
+
+companies5 = ('above-average','nielsen','cbs','sony','21st-century-fox','freewheel','comcast')
+
+companies6 = ('uninterrupted','espn','nba','tnt','aol','activision','cnn-com',
+'fanduel','cartoon-network','adult-swim','warner-bros-interactive-entertainment',
+'vaporchat','locket','videogram','dish-network','media-camp',
+'digitaslbi','mediabrands','mlb-advanced-media','engadget','buzzfeed','the-frisky','sezmi',
+'nba-tv','chili','t-mobile','turbine','gamesbeat','samsung-electronics',
+'tmz','dc-comics')
+
+companies7 = ('applause','magic-leap','wanda-group','legendary-entertainment',
+'paramount-pictures','stx-entertainment','the-cw-network','dreamworks-studios',
+'pixar','amblin-entertainment-i-connexion','oculus-vr','whatsapp','instagram',
+'universal-studios','insightpool','datasift','daydream','quickflix',
+'foxtel-management-pty-ltd','directv','kaltura','marvel-entertainment','lions-gate-entertainment-corp','starz','apple-tv','amazon-fire-tv',
+'vimeo','bittorrent','otoy')
+
+companies8 = ('philo','funny-or-die','bamtech', 'playbuzz','atom-tickets',
+'naritiv','jaunt','orbotix','statmuse','pley','maker-studios','babble','rocket-pack',
+'playdom','tapulous','kaboose','kerpoof','fanlib','ideal-bite','nowthis-news')
+
+companiesSalesforceAcquisitions= ('datorama','rebel','mulesoft','zorap','cloudcraze-llc',
+'attic-labs','krux','stypi','dimdim','choicepass','thinkfuse','prior-knowledge',
+'model-metrics','relateiq','jigsaw','sendia','kieden','kenlet','koral','instranet','groupswim',
+'informavores','sitemasher','activa-live','heroku','etacts',
+'manymoon','radian6','model-metrics','rypple','stypi','buddymedia','choicepass',
+'thinkfuse','goinstant','entropysoft','clipboard','exacttarget',
+'edgespring','toopher','tempo','akta','minhash','steelbrick','prediction-io',
+'implisit','demandware', 'coolan','quip','beyondcore','gravitytank','twin-prime-inc',
+'sequence')
+
+## attic-labs, kenlet did not run
+
+#cant find 'navajo-security' 'blue-tail'
+
+companiesSalesforcePartners = ('google','ibm','apple','twitter','facebook')
+
+# google got to i=111 04-23-2018 earliest article
+
+# according to gartner and other CRM altnernative sites
+# https://www.gartner.com/reviews/market/crm-customer-engagement-center/vendor/salesforce/alternatives
+companiesSalesforceCompetitors = ('zendesk',
+'service-now-com','infor', 'zoho','crmnext','pegasystems','genesys','talisma', 'Aptean', 'freshworks',
+'logmein','netsuite','mothernode','nimble-crm','infusionsoft',
+'sugarcrm','planplus-online','bpmonline','pipelinersales-corporation','workbooks','leadmaster',
+'insightly','prophet', 'goldmine-software', 'tableau', 'oracle','adobe','sap','microsoft', 'linkedin')
+
+## SAP only went to i=119 or 10-01-2011 earliest article date
+## microsoft went to i=110 or 01-26-2018 earliest article date, started at 110 went to 225
+## linkedin went to i=118 or 07-21-2016 earliest article date
+
+companiesSalesforceClients = ('keurig','anheauser-busch', 'turner', 'warner-bros',
+'marriot','adidas')
+
+# 2010 onward acquisitions
+companiesOracleAcquisitions = ('datascience-inc-','vocado','grapeshot','zenedge','aconex',
+'moat','wercker','apiary','dyn','apprity','logfire','netsuite','opower','textura',
+'crosswise','ravello-systems','addthis','stackengine','maxymiser','cloudmonkey',
+'datalogix','front-porch-digital','toa-technologies','micros-systems','live-look',
+'greenbytes','bluekai','corente','responsys','nirvanix','bitzer-mobile','bigmachines',
+'compendium-blogware', 'tekelec-international','nimbula','acme-packet','eloqua','dataraker','instantis',
+'selectminds','xsigo','skire','involver','collective-intellect','vitrue',
+'cleartrial','taleo','rightnow-technologies','endeca','goahead-software','inquira',
+'ksplice','fatwire-software','pillar-data-systems','datanomic','atg-arttechnologygroup',
+'prepaid-software','market2lead','secerno','phase-forward','amberpoint','convergin',
+'sun-microsystems','silver-creek-systems')
+
+## Oracle acquisitions cant find prepaid software
+
+#2010 onward acquisitions adobe
+#2010 onward acquisitions SAP
+#2010 onward acquisitions microsoft
