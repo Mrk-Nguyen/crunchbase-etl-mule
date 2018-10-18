2a199b430c1b913ea945a5d759d3eda8643a18f7
Mark Nguyen <Mrk-Nguyen@users.noreply.github.com>
2018-10-17 20:01:00 -0700

    Add helper functions to get total_page size

--- preprocess/crunchCompanies.py
+++ preprocess/crunchCompanies.py
@@ -1,27 +1,28 @@
 companies = ('marketo','sitecore','sailthru','agilone','searchignite','neustar',
-'tealium','ensighten','v12-group','terdata','idg-ventures','forrester-research',
+'tealium','ensighten','v12-group','teradata','idg-ventures','forrester-research',
 'the-walt-disney-company','mondelez-international','edmunds-com','eyeota','stirista',
 'conagra-inc','interactive-advertising-bureau-iab','nikkei','adroll-semantic-sugar-inc',
-'demandmedia','a-e-television-networks','vox-media','gartner','jetblue-airways','kellogg'
+'demandmedia','a-e-television-networks','vox-media','gartner','jetblue-airways','kellogg',
+'kimpton-hotel-restaurant-group','kiva','macys'
 )
 
 companies2 = ('bazaarvoice','centro','digital-trends','e-trade','financial-times',
 'foxtel-management-pty-ltd','the-guardian','dailymotion')
 
-companies3 = ('turner-broadcasting','warner-bros','roku','sling-com','fandango',
+companies3 = ('turner-broadcasting','warner-bros','roku','sling-3','fandango',
 'flixster','dramafever','bleacher-report','istreamplanet','refinery29','mashable')
 
-companies4 = ('le-figaro','keurig','mitsubishi','rea-group',
-'shutterstock','the-enthusiast-network','time','tribunecompany','wenner-media',
+companies4 = ('le-figaro','keurig-2','mitsubishi','rea-group',
+'shutterstock','grind-networks','time','tribunecompany','wenner-media',
 'whosay','wikia','choicestream','videoamp','admobius','idg-enterprise',
 'accel-partners','firefox')
 
 companies5 = ('above-average','nielsen','cbs','sony','21st-century-fox','freewheel','comcast')
 
-companies6 = ('uninterrupted','espn','nba','tnt','aol','activision','cnn-com',
+copanies6 = ('uninterrupted','espn','nba','aol','activision','cnn-com',
 'fanduel','cartoon-network','adult-swim','warner-bros-interactive-entertainment',
-'vaporchat','locket','videogram','dish-network','media-camp',
-'digitaslbi','mediabrands','mlb-advanced-media','engadget','buzzfeed','the-frisky','sezmi',
+'locket','videogram','dish-network','media-camp',
+'digitas','mediabrands','mlb-advanced-media','engadget','buzzfeed','the-frisky','sezmi',
 'nba-tv','chili','t-mobile','turbine','gamesbeat','samsung-electronics',
 'tmz','dc-comics')
 
@@ -29,7 +30,7 @@ companies7 = ('applause','magic-leap','wanda-group','legendary-entertainment',
 'paramount-pictures','stx-entertainment','the-cw-network','dreamworks-studios',
 'pixar','amblin-entertainment-i-connexion','oculus-vr','whatsapp','instagram',
 'universal-studios','insightpool','datasift','daydream','quickflix',
-'foxtel-management-pty-ltd','directv','kaltura','marvel-entertainment','lions-gate-entertainment-corp','starz','apple-tv','amazon-fire-tv',
+'foxtel-management-pty-ltd','directv','kaltura','marvel-entertainment','lions-gate-entertainment-corp','starz',
 'vimeo','bittorrent','otoy')
 
 companies8 = ('philo','funny-or-die','bamtech', 'playbuzz','atom-tickets',
@@ -37,22 +38,25 @@ companies8 = ('philo','funny-or-die','bamtech', 'playbuzz','atom-tickets',
 'playdom','tapulous','kaboose','kerpoof','fanlib','ideal-bite','nowthis-news')
 
 companiesSalesforceAcquisitions= ('datorama','rebel','mulesoft','zorap','cloudcraze-llc',
-'attic-labs','krux','stypi','dimdim','choicepass','thinkfuse','prior-knowledge',
+'attic-labs','krux-digital','stypi','dimdim','choicepass','thinkfuse','prior-knowledge',
 'model-metrics','relateiq','jigsaw','sendia','kieden','kenlet','koral','instranet','groupswim',
 'informavores','sitemasher','activa-live','heroku','etacts',
 'manymoon','radian6','model-metrics','rypple','stypi','buddymedia','choicepass',
 'thinkfuse','goinstant','entropysoft','clipboard','exacttarget',
 'edgespring','toopher','tempo','akta','minhash','steelbrick','prediction-io',
 'implisit','demandware', 'coolan','quip','beyondcore','gravitytank','twin-prime-inc',
-'sequence')
+'sequence','metamind', 'rebel-mind','akta-web-studio','akta-web-studio','gravitytank','pardot')
 
-## attic-labs, kenlet did not run
+## added companies after sequence
 
-#cant find 'navajo-security' 'blue-tail'
+# cant find for salesforce acquisitions
+#'navajo-security'
+#'blue-tail'
+#'kenlet'
 
-companiesSalesforcePartners = ('google','ibm','apple','twitter','facebook')
+companiesSalesforcePartners= ('google','ibm','apple','twitter','facebook')
 
-# google got to i=111 04-23-2018 earliest article
+# google got to i=111 04-23-2018 earliest article (send attempt to google tale i=121 with 4-11-2018)
 
 # according to gartner and other CRM altnernative sites
 # https://www.gartner.com/reviews/market/crm-customer-engagement-center/vendor/salesforce/alternatives
@@ -66,11 +70,27 @@ companiesSalesforceCompetitors = ('zendesk',
 ## microsoft went to i=110 or 01-26-2018 earliest article date, started at 110 went to 225
 ## linkedin went to i=118 or 07-21-2016 earliest article date
 
+# featured clients
+#https://www.salesforce.com/customer-success-stories/#!page=1
 companiesSalesforceClients = ('keurig','anheauser-busch', 'turner', 'warner-bros',
-'marriot','adidas')
+'marriot','adidas','schneider-electric','kone','us-bank','amazon-web-services',
+'farmers-insurance-group','red','transamerica','design-within-reach','t-mobile',
+'surf-air','yeti-coolers','adp','merck-co-inc','aldo','activision','canon','kimberly-clark',
+'philips-applied-technologies','philips-healthcare','ucsf-medical-center','ucsf-benioff-children-s-hospital',
+'meredith','timewarner','toyota','dufl','americanexpress','donorschoose-org',
+'intuit','american-red-cross','careerbuilder-com','scaled-agile','rosetta-stone',
+'t3-advisors','vodafone','teach-for-america','united-capital','mobilityworks',
+'bmc-software','papa-murphy-s-international-llc','dominos-pizza','loreal','spotify',
+'belly-inc','hoteltonight','dunkin-donuts','dunkin-brands-group','victor',
+'virgin-media','virgin-america','britannica-knowledge-systems','encyclopedia-britannica',
+'room-to-read','western-union','2U','ascap','algonquin-college','amica-mutual-insurance-co',
+'amtrak','angies-list','arizona-state-university','asana','aston-martin-lagonda-ltd',
+'lamborghini','rodan-fields-llc','barclays-plc','belkin-international','box',
+'brunello-cucinelli','caesars-entertainment','cisco','citrix','docusign','electronicarts',
+'general-electric','hersheys','ben-jerrys','intuitive-surgical','john-hancock')
 
-# 2010 onward acquisitions
-companiesOracleAcquisitions = ('datascience-inc-','vocado','grapeshot','zenedge','aconex',
+# 2010 Oracle onward acquisitions
+compainesOracleAcquisitions = ('datascience-inc-','vocado','grapeshot','zenedge','aconex',
 'moat','wercker','apiary','dyn','apprity','logfire','netsuite','opower','textura',
 'crosswise','ravello-systems','addthis','stackengine','maxymiser','cloudmonkey',
 'datalogix','front-porch-digital','toa-technologies','micros-systems','live-look',
@@ -84,6 +104,59 @@ companiesOracleAcquisitions = ('datascience-inc-','vocado','grapeshot','zenedge'
 
 ## Oracle acquisitions cant find prepaid software
 
-#2010 onward acquisitions adobe
-#2010 onward acquisitions SAP
-#2010 onward acquisitions microsoft
+## Adobe acquisitions full list 1990 start
+adobeAcquisitions = ['marketo','magento','sayspring', 'tubemogul', 'livefrye','mixamo',
+'fotolia','aviary','neolane','behance-inc','efficient-frontier-technology',
+'auditude','typekit','nitobi','iridas-technology','echosign','demdex','day-software',
+'omniture','business-catalyst','yawah','virtual-ubiquity','scene7','serious-magic-inc',
+'interakt','pixmantec','trade-and-technologies-france','navisware','macromedia',
+'okyz','qlink-technologies','yellow-dragon-software','syntrillium-software','accelio',
+'fotiva','go-live-systems','digidox','hyway-ferranti','sandcastle','ares-software',
+'frame-technology','photoshop','lastertools','aldus','ocr-systems','lastertools-language-tech',
+'bluepoint-technolgies','emerald-city-software']
+
+# 2000 onward
+microsoftAcquisitions = ['visio-corporation','peach-networks','travelscape','titus-communications',
+'bungie','netgames','mongo-music','pacific-microsonics','vacationspot','great-plains-software',
+'intellisol-int','ncompass-labs','maximal-innovative-intelligence', 'yupi','classic-custom-vacations',
+'sales-management-systems','navision','mobilicity','xdegrees','rare','vicinity','connectix',
+'DCG','placeware','ga-sullivan','gecad-software','3DO-Co-High-heat-baseball',
+'encore-bus-solutions','activeviews','lookoutsoftware','giant-company-software',
+'entegrate','groove-networks','messagecast','tsinghua-shenxun-cert','sybari-software',
+'teleo','frontbridge-technologies','alacris','media-streams','5th-finger','umt-software',
+'motionbridge','seadragon-software','apptimum','onfolio','lionhead-studios',
+'assetmetrix','massive-incorporated','vexcel','deepmetrix','proclarity','iview-multimedia',
+'softricity','winternals-software','whale-communications','gteko','desktop-standard',
+'colloquis','medstory','devBiz-business-solutions','screen-tonic','tellme-networks',
+'softartisans','engyro','stratature','savvis-inc','adECN','aQuantive','jellyfish',
+'parlano','global-care-solutions','HOB-business-solutions','musiwave','multimap',
+'calista-technologies','caligari-corp','yadata','rapt','komoku','90degree-software',
+'farecast','danger','fastsearchtransfer','kidaro','quadreon','navic-networks',
+'mobicomp','powerset','DATAllegro','greenfield-online','3DV-systems','bigpark',
+'rosetta-biosoftware','interactive-supercomputing','opalis-software','sentillion',
+'avicode','canesta','skype','prodiance','videosurf','yammer','perceptive-pixel',
+'phonefactor','storsimple','marketingplot','id8group-R2studios','pando-networks',
+'metricshub','netbreeze','inrelease','nokia','in-release','hlw-software','apiphany',
+'parature','greenbutton','capptain','syntaxtree','inmage','inception-mobile',
+'mojang','aorato','acompli','hockeyapp','equivio','revolution-analytics','sunrise-atelier',
+'n-trig','liveloop','datazen-software','6Wunderkinder-gmbh','bluestripe-software',
+'fieldone-systems','adallom','incent-games','volometrix','doublelabs','adxstudio-inc',
+'telekinesys','mobile-data-labs','secure-islands-technologies','metanautix','talko',
+'teacher-gaming','touchtype','groove','xamarin','solair','beam','genee','linknedin',
+'maluuba','simplygon','deis','intentional-software','hexadite','cloudyn','cycle-computing',
+'altspaceVR','SWNG','playfab','semantic-machines','github','ninja-theory','undead-labs',
+'compulsion-games', 'playground-games','flipgrid','bonsai','lobe','glint']
+
+
+# 2000 onward acquisitions SAP
+SAPAcquisitions = ['coresystems','callidus-cloud','recast.ai','gigya','abakus',
+'plat-one','altiscale','fedem-technology','roambi','concur','seewhy','fieldglass',
+'kxen','hybris','camilion','smartops','ticket-web','ariba','syclo','datango',
+'success-factors','right-hemisphere','crossgate','secude','cundus','sybase','technidata',
+'SAF','Highdeal','Visiprise','Saicon-inc','business-objects','yasu-technologies',
+'wicom-communications','maxware','outlooksoft','pilot-software','factory-logic',
+'praxis-software-solutions','frictionless-commerce','virsa-systems','sap-systems-integration',
+'callixa','khimetrics','triversity','lighthammer','dcs-quantum','tommorow-now',
+'ilytix','A2i','SPM-technologies','DCW-software','guimachine','imhc','expression',
+'topmanage','paynet-international','copa-gmbH','infinte-data-structures','toptier',
+'prescient-consulting','in-q-my-technologies-gmbh']
